f41b7040a6b656510401710ba982ba1b6addb2f9
Nishant Chaudhary <chaudharynishant025@gmail.com>
2019-07-16 18:32:07 -0500

    w3 solutions

---
+++ Basic Part-1/divideTwoNums.java
@@ -0,0 +1,10 @@
+//Write a Java program to divide two numbers and print on the screen.
+
+public class Main {
+
+    public static void main(String[] args) {
+        int num1 = 60;
+        int num2 = 20;
+        System.out.println("Divide Result: " + (num1 / num2));
+    }
+}
