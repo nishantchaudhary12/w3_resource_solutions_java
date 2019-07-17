dd5179f37a4c06a7762e3d71b5454fd4d67d57be
Nishant Chaudhary <chaudharynishant025@gmail.com>
2019-07-17 02:06:39 -0500

    w3 solutions

---
+++ Basic Part-1/sumTwoNums.java
@@ -0,0 +1,10 @@
+//Write a Java program to print the sum of two numbers.
+
+public class Main {
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        System.out.println("Sum: " + (num1 + num2));
+    }
+}
