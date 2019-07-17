44f714a3dd8f8f84fdf9434886a9b8c43e7e609e
Nishant Chaudhary <chaudharynishant025@gmail.com>
2019-07-17 02:06:57 -0500

    w3 solutions

---
+++ Basic Part-1/rectangleAreaAndPerimeter.java
@@ -0,0 +1,11 @@
+//Write a Java program to print the area and perimeter of a rectangle.
+
+
+public class demo{
+    public static void main(String[] args) {
+        double len = 10d;
+        double br = 8d;
+        System.out.println("Area: " + (len * br));
+        System.out.println("Perimeter: " + (2*(len + br)));
+    }
+}
