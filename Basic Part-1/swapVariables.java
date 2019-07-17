e397869589cfdcc96d3a01cc6538c4557179fb33
Nishant Chaudhary <chaudharynishant025@gmail.com>
2019-07-17 02:06:50 -0500

    w3 solutions

---
+++ Basic Part-1/swapVariables.java
@@ -0,0 +1,16 @@
+//Write a Java program to swap two variables.
+
+
+public class demo{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int temp;
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println("a: " + a + " b: " + b);
+
+        }
+    }
+
