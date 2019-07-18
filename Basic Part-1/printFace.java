0cd0856f775a9cceed01dc91a6debea30146ca44
Nishant Chaudhary <chaudharynishant025@gmail.com>
2019-07-17 21:11:32 -0500

    w3 solutions

---
+++ Basic Part-1/printFace.java
@@ -0,0 +1,14 @@
+//Write a Java program to print a face.
+
+
+public class demo{
+    public static void main(String[] args) {
+        System.out.println(" +\"\"\"\"\"+ ");
+        System.out.println("[| o o |]");
+        System.out.println(" |  ^  | ");
+        System.out.println(" | '-' | ");
+        System.out.println(" +-----+ ");
+
+        }
+    }
+
