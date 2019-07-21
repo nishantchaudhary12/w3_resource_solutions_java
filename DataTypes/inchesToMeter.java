6c3d888bac57fef2b33bc5b9e65c2863075f4ca7
Nishant Chaudhary <chaudharynishant025@gmail.com>
2019-07-20 21:18:25 -0500

    w3 solutions

---
+++ DataTypes/inchesToMeter.java
@@ -0,0 +1,11 @@
+//Write a Java program that reads a number in inches, converts it to meters.
+
+
+public class demo{
+    public static void main(String[] args) {
+        double inch = 1000d;
+        double meters = inch * 0.0254d;
+        System.out.println(inch + " inches = " + meters + " meters");
+        }
+    }
+
